040d74cd5f24484f671e21b321e4ae2c14d90778
Burak Kiper <30374154+delphi911@users.noreply.github.com>
2021-04-13 17:04:00 +0300

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative 'hangman.rb'
+require_relative 'text_content.rb'
+require_relative 'text_instructions.rb'
+require_relative 'words.rb'
+require_relative 'display.rb'
+require_relative 'savegame.rb'
+Game.new.play
